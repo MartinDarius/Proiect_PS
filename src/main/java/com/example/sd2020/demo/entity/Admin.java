4573f79a7a71a8d8ab74d0acbb6aedb0b569fadb
martindarius <martin.darius1998@yahoo.com>
2020-03-29 23:30:05 +0300

    observer

--- src/main/java/com/example/sd2020/demo/entity/Admin.java
+++ src/main/java/com/example/sd2020/demo/entity/Admin.java
@@ -48,7 +48,7 @@ public class Admin implements Observator {
     public void setEmail(String email) {
         this.email = email;
     }
-
+    //metoda care se suprascrie
     @Override
     public void update(Object news) {
         this.setNews((String) news);
